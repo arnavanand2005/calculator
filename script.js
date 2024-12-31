b90e3484f37055dc27f8735bad7fc5c53e90e624
arnavanand2005 <aa0978@srmist.edu.in>
2024-12-31 22:16:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ display=document.getElementById("display");
+ 
+ function appendToDisplay(input){
+    display.value+=input;
+ }
+
+ function clearDisplay(){
+    display.value="";
+ }
+
+ function calculate(){
+    display.value=eval(display.value);
+ }
